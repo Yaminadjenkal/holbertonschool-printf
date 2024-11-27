b8a7abd9bd9e5a8c32c0259f84a907175405eef1
Zeynab <zsakho58@gmail.com>
2024-11-27 12:47:06 +0100

    Function that prints characters

---
+++ print_char.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+/*
+ * File: print_char.c
+ *
+ */
+
+
+/**
+ *_printf - Custom implementation of the printf function for formatted output.
+ * @format: A null-terminated string that specifies the format of the output.
+ *
+ *
+ */
+
+
+void _printf(const char *format, ...)
+{
+	char value;
+	va_list args;
+
+	va_start(args, format);
+
+
+	while (*format)
+	if (*format == '%')
+	{	format++;
+	}
+
+	if (*format == 'c')
+	{	char value = (char)va_arg(args, int);
+
+		putchar(value);
+
+		else
+			putchar(*format);
+	}
+	format++;
+
+
+	va_end(args);
+}
