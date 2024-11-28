6d2c1329d86f98bbf4e2453a7cfd177c5bf012a0
Yaminadjenkal <mina20101951@hotmail.com>
2024-11-28 15:38:56 +0000

    add a description for the file

--- print_string.c
+++ print_string.c
@@ -1,11 +1,17 @@
 #include "main.h"
+/**
+ * print_string - Function that prints a string
+ * @args: Argument list containing the string to be printed
+ * Return: The number of characters printed.
+ */
+int print_string(va_list args)
+{
+	char *str = va_arg(args, char*);
+	int count = 0;
 
-int print_string(va_list args) {
-    char *str = va_arg(args, char*);
-    int count = 0;
-
-    while (str && str[count]) {
-        count += write(1, &str[count], 1);
-    }
-    return (count);
+	while (str && str[count])
+	{
+		count += write(1, &str[count], 1);
+	}
+	return (count);
 }
