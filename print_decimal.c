6b865461b68afc6fff4874ba65b65284a815912a
Yaminadjenkal <mina20101951@hotmail.com>
2024-11-28 21:29:07 +0000

    fix all my files

---
+++ print_decimal.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_decimal - Function that prints a decimal (signed integer)
+ * @args: Argument list containing the integer to be printed
+ * Return: The number of characters printed
+ */
+int print_decimal(va_list args)
+{
+    int n = va_arg(args, int);
+    char buffer[50];
+    int i = 0, count = 0;
+
+    if (n == 0)
+        return write(1, "0", 1);
+
+    if (n < 0)
+    {
+        buffer[i++] = '-';
+        n = -n;
+    }
+
+    while (n > 0)
+    {
+        buffer[i++] = (n % 10) + '0';
+        n /= 10;
+    }
+
+    for (int j = i - 1; j >= 0; j--)
+        count += write(1, &buffer[j], 1);
+
+    return count;
+}
+
