88ac1d414f2aef571d5858e921e017771900276e
Yaminadjenkal <mina20101951@hotmail.com>
2024-11-28 11:23:44 +0000

    fix all my files

--- main.h
+++ main.h
@@ -1,12 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdarg.h>
-int _printf(const char *format, ...);
-int _printf_modulo(const char *format, ...);
-int print_d(va_list args);
-void convert_to_string(int num, char *buffer, int len);
-int calculate_length(int num);
-int handle_negative(int num, char *buffer, int *len);
-int print_i(va_list args);
 
+#include <stdarg.h>
+
+int handle_negative(int num, char *buffer, int *len);
+int calculate_length(int num);
+void convert_to_string(int num, char *buffer, int len);
+int print_d(va_list args);
+int print_i(va_list args);
+void print_string(const char *str, int *count);
+void print_invalid_format(char specifier, int *count);
+int _printf_string(const char *format, ...);
 #endif /* MAIN_H */
+
