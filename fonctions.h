e7ab0ca235bba81636fb53c536bc353940d6cf51
Yaminadjenkal <mina20101951@hotmail.com>
2024-11-28 11:16:39 +0000

    fix all my files

--- fonctions.h
+++
@@ -1,9 +0,0 @@
-#ifndef FONCTIONS_H
-#define FONCTIONS_H
-
-int handle_negative(int num, char *buffer, int *len);
-int calculate_length(int num);
-void convert_to_string(int num, char *buffer, int len);
-
-#endif /* FONCTIONS_H */
-
