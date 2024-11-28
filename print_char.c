f7ffafd585625352927cb280cb3292bbb632547f
Zeynab <zsakho58@gmail.com>
2024-11-28 13:39:46 +0100

    Description Update

--- print_char.c
+++ print_char.c
@@ -1,6 +1,20 @@
 #include "main.h"
+/*
+ * File: print_char.c
+ *
+ * Description: Implements the `print_char` function to print a character
+ * from a variadic argument list.
+ */
 
-int print_char(va_list args) {
-    char c = va_arg(args, int);
-    return write(1, &c, 1);
+/**
+ * print_char - Prints a character to standard output.
+ * @args: The variadic argument list.
+ * Return: 1 if successful, -1 on error.
+ */
+
+int print_char(va_list args)
+{
+	char c = va_arg(args, int);
+
+	return (write(1, &c, 1));
 }
